bc83685770afc696011db08000d08ae303f6eb6b
hoonzinope <hoonzinope@gmail.com>
2025-03-29 12:08:21 +0900

    first commit

---
+++ src/test/java/com/example/searchWorker/SearchWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchWorker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
